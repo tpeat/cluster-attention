1ab7fc19dd979d62c77bec5ff4dfa0809a609d65
tristan.j.peat@gmail.com <tpeat3@gatech.edu>
2024-10-06 22:12:43 -0400

    Ensure all tokenizers are multilingual and support french

--- machine-translation/data/tokenizer.py
+++ machine-translation/data/tokenizer.py
@@ -2,11 +2,11 @@ from transformers import BertTokenizer, MBartForConditionalGeneration, MBartToke
 
 def make_tokenizer(name="MBart"):
     if name == "Bert":
-        tokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-uncased")
+        tokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-multilingual-cased")
     elif name == "MBart":
-        tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-en-ro", src_lang="en_XX", tgt_lang="ro_RO")
+        tokenizer = MBartTokenizer.from_pretrained("facebook/mbart-large-50-many-to-many-mmt", src_lang="en_XX", tgt_lang="fr_XX")
     elif name == "opus":
-        tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-nl")
+        tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-fr")
     else:
         print("Invalid tokenizer name")
     return tokenizer
