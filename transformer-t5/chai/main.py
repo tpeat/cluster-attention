1d23c48f73ff9a7fb9d879b290d6a5337e2e39a6
sophieimhof <91424360+sophieimhof@users.noreply.github.com>
2024-12-07 12:36:49 -0500

    Update main.py

--- transformer-t5/chai/main.py
+++ transformer-t5/chai/main.py
@@ -1,17 +1,21 @@
 from model import load_pretrained_model
 from clustering import create_clusters, assign_weights_to_clusters
-from plotting import plot_heads
+from plot_results import plot_heads, plot_head
 
 def cluster_head_weights():
     model, tokenizer = load_pretrained_model()
-    num_clusters = 80
+    num_clusters = 100
     head_groups = create_clusters(model, tokenizer, num_clusters)
-
     assign_weights_to_clusters(model, head_groups, num_clusters)
-
-    # Save the updated model
+    #Save the updated model
     modified_checkpoint_path = "modified_checkpoint_per_cluster_80"
     model.save_pretrained(modified_checkpoint_path)
     print(f"Modified checkpoint saved to: {modified_checkpoint_path}")
 
 cluster_head_weights()
+
+
+#plot a specific attention head
+#model, tokenizer = load_pretrained_model()
+#plot_head(model, tokenizer, 8, 11, plot_name="plots/cluster-2-head-2.png")
+
